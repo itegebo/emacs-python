e608fbfd50650f12cfe454f57437c63723dbf2c3
Ian Tegebo <ian.tegebo@gmail.com>
2011-02-15 23:30:39 -0800

    This seems to be a working setup for using ipython with python-mode, while ipython completions seem to work due to Yaroslav Halchenko's workaround.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,20 +1,31 @@
-
+;; tegebo's theme of the moment
 (require 'color-theme)
 (setq color-theme-is-global t)
 (color-theme-charcoal-black)
 
 ;; python-mode.el
-(setq load-path
-      (append (list nil
-		    "~/.emacs.d/major-modes/python-mode-5.2.0"
-		    )
-	      load-path))
-
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                   interpreter-mode-alist))
+(add-to-list 'load-path "~/.emacs.d/major-modes/python-mode-5.2.0")
+(add-to-list        'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python"   . python-mode))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 (global-font-lock-mode t)
 (setq font-lock-maximum-decoration t)
+;;need pymacs for (require 'pycomplete) and there may be better options
 
-;;(require 'python-mode)
+;; ipython.el
+(add-to-list 'load-path "~/.emacs.d/lib/elisp")
+(require 'ipython)
+;; Workaround for completions in 0.10
+;; http://mail.python.org/pipermail/python-mode/2011-January/000888.html
+(setq ipython-completion-command-string
+      "print(';'.join(__IP.Completer.all_completions('%s'))) #PYTHON-MODE SILENT\n")
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
